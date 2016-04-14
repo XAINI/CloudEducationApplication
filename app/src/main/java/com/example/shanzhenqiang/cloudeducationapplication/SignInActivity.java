5a20addd8df6ed77065204949dc1a541e7d9439d
XAINI <shanzhenqiang0814@163.com>
2016-04-14 18:28:10 +0800

    This push I finished sign up and sign in

--- app/src/main/java/com/example/shanzhenqiang/cloudeducationapplication/SignInActivity.java
+++ app/src/main/java/com/example/shanzhenqiang/cloudeducationapplication/SignInActivity.java
@@ -1,9 +1,30 @@
 package com.example.shanzhenqiang.cloudeducationapplication;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -11,12 +32,107 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        Button btn = (Button) findViewById(R.id.signInBtnId);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MySignInTask signInTask = new MySignInTask();
+                signInTask.execute();
+            }
+        });
     }
 
-    public void signIn(View view){
-        Intent intent = new Intent(this, CloudEducationHomeActivity.class);
-        startActivity(intent);
+
+
+//  登录的 Task
+    public class MySignInTask extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String result;
+            try {
+                result = get_uer_info();
+                return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            ArrayList<HashMap<String, Object>> listItem = formatConvert(result);
+            boolean compareResult = verification(listItem);
+            if (compareResult == true){
+                Intent intent = new Intent(SignInActivity.this, CloudEducationHomeActivity.class);
+                startActivity(intent);
+            }else{
+                TextView textView = (TextView) findViewById(R.id.SignInPromptId);
+                textView.setText("您的账号与密码不符");
+            }
+        }
+
+        // 验证用户信息（用户名和密码是否存在）
+        public boolean  verification(ArrayList<HashMap<String, Object>> listItem){
+            EditText accountId = (EditText) findViewById(R.id.signInAccountId);
+            EditText pwdId = (EditText) findViewById(R.id.signInPwdId);
+            HashMap<String, Object> map = new HashMap<String, Object>();
+
+            String account = accountId.getText().toString();
+            String pwd = pwdId.getText().toString();
+            map.put("name", account);
+            map.put("password", pwd);
+            if (listItem.contains(map))
+                return true;
+            else
+                return false;
+        }
+        // 格式转换 将返回的 json 转换成 ArrayList
+        public ArrayList<HashMap<String, Object>> formatConvert(String result) {
+            ArrayList<HashMap<String, Object>> listItem = new ArrayList<HashMap<String, Object>>();
+            try {
+                JSONArray jsonArray = new JSONArray(result);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    HashMap<String, Object> map = new HashMap<String, Object>();
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String name = jsonObject.getString("name");
+                    String pwd = jsonObject.getString("password");
+                    map.put("name", name);
+                    map.put("password", pwd);
+                    listItem.add(map);
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return listItem;
+        }
+
     }
+
+    String get_uer_info() throws Exception {
+        String url = "http://192.168.100.3:3000/users/fetch_users_info";
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
+        Response response = client.newCall(request).execute();
+        if (response.isSuccessful()) {
+            return response.body().string();
+        } else {
+            throw new IOException("Unexpected code " + response);
+        }
+
+    }
+
+
 
     public void backRegisterPage(View view){
         Intent intent = new Intent(this, SignUpActivity.class);
--- app/src/main/java/com/example/shanzhenqiang/cloudeducationapplication/SignUpActivity.java
+++ app/src/main/java/com/example/shanzhenqiang/cloudeducationapplication/SignUpActivity.java
@@ -5,48 +5,28 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import okhttp3.FormBody;
-import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class SignUpActivity extends AppCompatActivity {
-    /*public final static String EXTRA_MESSAGE = "com.example.myapplication.MESSAGE";*/
+    public final static String EXTRA_MESSAGE = "com.example.myapplication.MESSAGE";
 
-    private DBManager mgr;
-    private ListView listView;
-    private TextView textView;
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-//        mgr = new DBManager(this);
-
-
-        //获取text中 用户填入的值
-        listView = (ListView) findViewById(R.id.listView);
+        // User sign up
         Button btn = (Button) findViewById(R.id.sign_up);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,7 +65,16 @@ public class SignUpActivity extends AppCompatActivity {
 
             TextView listView = (TextView) findViewById(R.id.thisTestView);
             listView.setText(result);
+            final Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
 
+            Timer timer = new Timer();
+            TimerTask task = new TimerTask() {
+                @Override
+                public void run() {
+                    startActivity(intent);
+                }
+            };
+            timer.schedule(task, 1000 * 1);
         }
 
         String post_to_service() throws IOException {
@@ -102,49 +91,9 @@ public class SignUpActivity extends AppCompatActivity {
             Request request = new Request.Builder().url(url).post(formBody).build();
             Response response = client.newCall(request).execute();
             return response.body().string();
-//            if (!response.isSuccessful())throw new IOException("Unexpected code " + response);
         }
 
     }
-
-
-    // test SQLite
-//    protected void onDestroy(){
-//        super.onDestroy();
-//        mgr.closeDB();
-//    }
-//
-//    public void signUpUser(View view){
-//        EditText editAccountText = (EditText) findViewById(R.id.accountEdittext);
-//        String account = editAccountText.getText().toString();
-//        EditText editPasswordText = (EditText) findViewById(R.id.pwdEdittext);
-//        String password = editPasswordText.getText().toString();
-//
-//        ArrayList<User> users = new ArrayList<User>();
-//
-//        User user = new User(account,password);
-//        users.add(user);
-//
-//        mgr.add(users);
-//
-//        Intent intent = new Intent(this, SignInActivity.class);
-//        startActivity(intent);
-//    }
-
-//    public void query(View view){
-//        List<User> users = mgr.query();
-//        ArrayList<Map<String, String>> list = new ArrayList<Map<String, String>>();
-//        for (User user : users){
-//            HashMap<String, String> map = new HashMap<>();
-//            map.put("account", user.account);
-//            map.put("password", user.password );
-//            list.add(map);
-//        }
-//
-//        SimpleAdapter adapter = new SimpleAdapter(this, list, android.R.layout.simple_list_item_2, new String[]{"account","password"}, new int[]{android.R.id.text1, android.R.id.text2});
-//        listView.setAdapter(adapter);
-//    }
-
 
     public void checkSignUp(View view){
         Intent intent = new Intent(this,SignInActivity.class);
